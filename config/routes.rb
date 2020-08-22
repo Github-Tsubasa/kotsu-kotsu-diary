a149e969dae1fbc8e88d95b7ad80aae3394626f1
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-08-22 15:44:37 +0900

    Implementation of delete function 20200822

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  resources :posts, only: [:new, :create, :show]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :posts, except: :index
+
 
 end
