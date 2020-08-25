6f107307bd98402017c5b9004b3ede2e3a303e25
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-08-25 09:32:31 +0900

    implementation of mypage 20200825

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = Post.includes(:user)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :posts, except: :index
+  resources :users, only: :show
 
 
 end
